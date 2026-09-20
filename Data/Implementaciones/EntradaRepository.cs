b698b51bf533ca6b6fcae0feb442013114cbccdf
G-rm4n <oligah06@gmail.com>
2026-09-19 22:38:39 -0300

    feat:Implementa EntradaRepository

--- Data/Implementaciones/EntradaRepository.cs
+++ Data/Implementaciones/EntradaRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Data.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,28 @@ using System.Threading.Tasks;
 
 namespace Data.Implementaciones
 {
-    internal class EntradaRepository
+    public class EntradaRepository:IEntradaRepository
     {
+        private readonly TPIContext _context;
+
+        public EntradaRepository(TPIContext context)
+        {
+            _context = context;
+        }
+        public async Task<Entrada?> ObtenerPorIdAsync(int idEntrada)
+        {
+            return await _context.Entradas.FirstOrDefaultAsync(e=>e.EntradaId == idEntrada);
+        }
+
+        public async Task<IEnumerable<Entrada>> ObtenerPorEventoAsync(int idEvento)
+        {
+            return await _context.Entradas.Where(e=>e.EventoId== idEvento).ToListAsync();
+        }
+
+        public async Task AddAsync(Entrada entrada)
+        {
+            _context.Add(entrada);
+            await _context.SaveChangesAsync();
+        }
     }
 }
---
+++ Data/Interfaces/IEntradaRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    internal interface IEntradaRepository
+    {
+        Task<Entrada?> ObtenerPorIdAsync(int id);
+        Task<IEnumerable<Entrada>> ObtenerPorEventoAsync(int eventoId);
+        Task AddAsync(Entrada entrada);
+    }
+}
